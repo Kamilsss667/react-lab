fb56710fb1c6e0adfa27f894b03db23525258c69
syrowy <syrowy@student.agh.edu.pl>
2024-05-18 11:55:20 +0200

    Trzeci etap

--- src/App.js
+++ src/App.js
@@ -5,27 +5,44 @@ import {useState} from "react";
 
 function App() {
     const [email, setEmail] = useState('fracz@agh.edu.pl');
-
+    const [isAuthenticated, setIsAuthenticated] = useState (false);
     function handleChange(event) {
         setEmail(event.target.value);
-    }
-    let dlugosc;
 
-    if (email.length < 5){
-        dlugosc = <div> Ale masz krótki adres! </div>;
-    } else if (email.length<10){
-        dlugosc = <div> Twój adres jest git </div>;
     }
-    else if(email.length>10) {
-        dlugosc = <div> za długi </div>;
+function logout(){
+        setIsAuthenticated(false);
+        setEmail(' ');
+
+}
+    let content;
+    if (isAuthenticated){
+        content = <div>
+            <h2> Witaj {email}! </h2>
+        <button onClick = {logout}>
+            Wyloguj
+        </button>
+        </div>
+    }
+    else
+    {
+        content = <div>
+            <h2> Zaloguj się swoim e-mailem</h2>
+            <input value = {email}
+                   type = "text"
+                   onChange ={handleChange}
+                   className = "dlugiepole"
+                   />
+            <button onClick={() => setIsAuthenticated(true)}>
+                Zaloguj się
+            </button>
+            </div>
     }
 
   return (
       <div>
-        <h1>System do zapisów na zajęcia</h1>
-        <h2>Twój e-mail to {email}</h2>
-          {dlugosc}
-        <input type="text" value= {email} onChange={handleChange}/>
+        <h1>Witaj w systemie do zapisów na zajęcia</h1>
+          {content}
       </div>
   );
 }
